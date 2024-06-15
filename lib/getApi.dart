054e9731346065890cd0f676d5ac440719634564
Aksh-001 <92627082+Aksh-001@users.noreply.github.com>
2024-06-15 10:48:25 +0530

    Corosual changed by AKshay Khairnar

--- lib/getApi.dart
+++ lib/getApi.dart
@@ -13,7 +13,7 @@ class GetApiWithListview extends StatefulWidget {
 
 class _GetApiWithListviewState extends State<GetApiWithListview> {
   List<dynamic> jsonList = [];
-  final CarouselController _carouselController = CarouselController();  // Carousel controller
+  // final CarouselController _carouselController = CarouselController();  // Carousel controller
 
   @override
   void initState() {
@@ -39,158 +39,173 @@ class _GetApiWithListviewState extends State<GetApiWithListview> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      appBar: AppBar(
-        title: const Text('Shopping',style: TextStyle(fontWeight: FontWeight.w600),),
-      ),
-      body:Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            SizedBox(height: 10,),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                height: 50,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                    color: Colors.grey,
-                  borderRadius: BorderRadius.circular(50)
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Icon(Icons.search),
-                      Text('Search any product or url'),
-                      Icon(Icons.camera_alt)
-                    ],
+        appBar: AppBar(
+          title: const Text(
+            'Shopping',
+            style: TextStyle(fontWeight: FontWeight.w600),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 50,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(50)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Icon(Icons.search),
+                        Text('Search any product or url'),
+                        Icon(Icons.camera_alt)
+                      ],
+                    ),
                   ),
                 ),
-
               ),
-            ),
-            SizedBox(height: 20,),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("We thik you will love these",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w600,),),
-                  IconButton(
-                    icon: Icon(Icons.arrow_forward_ios_rounded),
-                    onPressed: (){},
-
-                  )
-                ],
+              SizedBox(
+                height: 20,
               ),
-            ),
-
-
-
-            Expanded(
-              child: CarouselSlider.builder(
-                itemCount: jsonList.length,
-                itemBuilder: (BuildContext context, int index, int realIndex) {
-                  var item = jsonList[index];
-                  return Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: SizedBox(
-                      height: 300, // Set the height of the card
-                      width: 300, // Set the width of the card
-                      child: IntrinsicHeight(
-                        child: Card(
-                          clipBehavior: Clip.hardEdge,
-                          child: Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                               Padding(
-                                 padding: const EdgeInsets.only(left: 0.0),
-                                 child: Container(
-                                   height: 40,
-                                   width: 40,
-                                   decoration: BoxDecoration(
-                                     color: Colors.pink,
-                                     shape: BoxShape.circle
-                                   ),
-                                   child: IconButton(
-                                     color: Colors.white,
-                                     icon: Icon(Icons.heart_broken_rounded),
-                                     onPressed: (){},
-                                   ),
-                                 ),
-                               ),
-                                Center(
-                                  child: Image.network(
-                                    item['image'],
-                                    width: 100,
-                                    height: 100,
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
-                                SizedBox(height: 20,),
-                                Center(
-                                  child: Text(
-                                    item['title'],
-                                    style: const TextStyle(
-                                        fontWeight: FontWeight.bold, fontSize: 15),
-                                  ),
-                                ),
-                                SizedBox(height: 20,),
-                                Row(
-                                  children: [
-                                    Icon(Icons.star,color: Colors.yellow,),
-                                    Text(
-                                      '${item['rating']['rate']}',
-                                      style: const TextStyle(
-                                          fontWeight: FontWeight.w600,
-                                          fontSize: 15,
-                                          color: Colors.black),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "We thik you will love these",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.arrow_forward_ios_rounded),
+                      onPressed: () {},
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                child: CarouselSlider.builder(
+                  itemCount: jsonList.length,
+                  itemBuilder:
+                      (BuildContext context, int index, int realIndex) {
+                    var item = jsonList[index];
+                    return Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: SizedBox(
+                        height: 300, // Set the height of the card
+                        width: 300, // Set the width of the card
+                        child: IntrinsicHeight(
+                          child: Card(
+                            clipBehavior: Clip.hardEdge,
+                            child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 0.0),
+                                    child: Container(
+                                      height: 40,
+                                      width: 40,
+                                      decoration: BoxDecoration(
+                                          color: Colors.pink,
+                                          shape: BoxShape.circle),
+                                      child: IconButton(
+                                        color: Colors.white,
+                                        icon: Icon(Icons.heart_broken_rounded),
+                                        onPressed: () {},
+                                      ),
                                     ),
-                                  ],
-                                ),
-                                SizedBox(height: 20,),
-                                 Row(
-                                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                   children: [
-                                     Text(
+                                  ),
+                                  Center(
+                                    child: Image.network(
+                                      item['image'],
+                                      width: 100,
+                                      height: 100,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 20,
+                                  ),
+                                  Center(
+                                    child: Text(
+                                      item['title'],
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 15),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 20,
+                                  ),
+                                  Row(
+                                    children: [
+                                      Icon(
+                                        Icons.star,
+                                        color: Colors.yellow,
+                                      ),
+                                      Text(
+                                        '${item['rating']['rate']}',
+                                        style: const TextStyle(
+                                            fontWeight: FontWeight.w600,
+                                            fontSize: 15,
+                                            color: Colors.black),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: 20,
+                                  ),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
                                         '₹${item['price']}',
                                         style: const TextStyle(
                                             fontWeight: FontWeight.w600,
                                             fontSize: 20,
                                             color: Colors.green),
                                       ),
-                                     IconButton(
-                                         icon:Icon(Icons.shopping_cart),
-                                     onPressed: (){},)
-                                   ],
-                                 ),
-                                      ],
+                                      IconButton(
+                                        icon: Icon(Icons.shopping_cart),
+                                        onPressed: () {},
+                                      )
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  );
-                },
-                options: CarouselOptions(
-                  height: 400,
-                  autoPlay: true,
-                  enlargeCenterPage: true,
-                  aspectRatio: 16 / 9,
-                  viewportFraction: 0.8,
+                    );
+                  },
+                  options: CarouselOptions(
+                    height: 400,
+                    autoPlay: true,
+                    enlargeCenterPage: true,
+                    aspectRatio: 16 / 9,
+                    viewportFraction: 0.8,
+                  ),
                 ),
-              ),
-            )
-
-          ],
-        ),
-      )
-
-    );
+              )
+            ],
+          ),
+        ));
   }
 }
